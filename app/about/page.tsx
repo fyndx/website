8fce615919a2fb156d78a37ef08d0ba5c8a7ee84
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2025-03-25 00:04:13 +0530

    fix: comment out Team and Timeline components in About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,8 +16,8 @@ export default function AboutPage() {
       <AboutHero />
       <Mission />
       <Values />
-      <Team />
-      <Timeline />
+      {/* <Team /> */}
+      {/* <Timeline /> */}
       <CTASection />
     </>
   )
